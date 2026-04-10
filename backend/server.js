d64c9dbd8d715a0e03c860a28c3b0cd3afb7d74d
Nevin alex varghese <143860561+neeeeevin@users.noreply.github.com>
2026-04-11 03:40:09 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -32,6 +32,6 @@ app.use((err, _req, res, _next) => {
 });
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
+app.listen(PORT, "0.0.0.0", () => {
   console.log(`Server running on port ${PORT}`);
 });
